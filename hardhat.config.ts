f916b334c559fd2b49ef6408aaaf76ef7aff42b7
Roy <r@o.com>
2022-03-29 19:15:12 +0300

    optmizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,12 +23,22 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.10",
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        process.env.ROPSTEN_PRIVATE_KEY !== undefined
+          ? [process.env.ROPSTEN_PRIVATE_KEY]
+          : [],
     },
   },
   gasReporter: {
